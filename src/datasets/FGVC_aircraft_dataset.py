c4b179f7d90e7b89b0134518033cb7a0619c8934
jackwarren430 <jackwarren430@gmail.com>
2026-02-26 18:42:04 -0500

    Boilerplate dataset/model files

---
+++ src/datasets/FGVC_aircraft_dataset.py
@@ -0,0 +1,13 @@
+import torch
+from torch.utils.data import Dataset
+
+class AircraftCaptionDataset(Dataset):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
