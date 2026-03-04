c26a0b4192ad1a127b56869f193ac5233ac9347b
Trent Doiron <trentwdoiron@gmail.com>
2026-03-04 17:07:53 -0500

    .gitignore update

--- src/datasets/FGVC_aircraft_dataset.py
+++ src/datasets/FGVC_aircraft_dataset.py
@@ -1,13 +1,20 @@
 import torch
 from torch.utils.data import Dataset
+from PIL import Image
+import os 
 
 class AircraftCaptionDataset(Dataset):
-    def __init__(self, **kwargs):
+    def __init__(self, image_paths, labels_path, **kwargs):
         super().__init__(**kwargs)
-        pass
+
+        DIR = os.path.dirname(os.path.abspath(__file__))
+        os.path.join(DIR, )
+
+        self.image_paths = image_paths
 
     def __len__(self):
         pass
 
     def __getitem__(self, index):
-        pass
+        img = Image.open(self.image_paths[index]).convert("RGB")
+        return img, 
