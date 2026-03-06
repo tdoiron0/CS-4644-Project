74800b529a44c08c7a474be98874642010e0ed3c
Trent Doiron <trentwdoiron@gmail.com>
2026-03-06 18:36:18 -0500

    dataset class implemented

--- src/datasets/FGVC_aircraft_dataset.py
+++ src/datasets/FGVC_aircraft_dataset.py
@@ -3,14 +3,16 @@ from torch.utils.data import Dataset
 from PIL import Image
 import os 
 import csv
+import numpy as np
+from transformers import AutoProcessor
 
 class AircraftCaptionDataset(Dataset):
-    def __init__(self, root_path, **kwargs):
+    def __init__(self, root_path, processor: AutoProcessor, **kwargs):
         super().__init__(**kwargs)
 
         self.root_path = root_path
-        self.images_path = os.path.join(root_path, "images")
-        self.labels_path = os.path.join(root_path, "labels.csv")
+        self.images_path = os.path.join(self.root_path, "images")
+        self.labels_path = os.path.join(self.root_path, "labels.csv")
         
         with open(self.labels_path, "r", newline="") as f:
             reader = csv.DictReader(f)
@@ -21,6 +23,14 @@ class AircraftCaptionDataset(Dataset):
 
     def __getitem__(self, index):
         datapoint = self.label_rows[index]
+
         img_path = os.path.join(self.images_path, datapoint["image_id"] + ".jpg")
-        img = Image.open(img_path).convert("RGB")
-        return img, datapoint["manufacturer"], datapoint["family"], datapoint["variant"]
+        img_raw = Image.open(img_path).convert("RGB")
+
+        label = f"The manufacturer is {datapoint["manufacturer"]}, the family is {datapoint["family"]}, the variant is {datapoint["variant"]}."
+
+        return self.processor(
+            text="What is the manufacturer, family, and variant of this aircraft", 
+            images=img_raw, 
+            return_tensors="pt"
+        ), self.processor.tokenizer(label, return_tensors="pt")
